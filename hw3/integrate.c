88418c68433c313eb8882b0ea5d7d9a09cf8a533
susu <u1240976@gmail.com>
2015-12-03 23:39:20 +0800

    hw3 feature code and makefile

--- hw3/integrate.c
+++ hw3/integrate.c
@@ -1,28 +1,59 @@
 #include <stdio.h>
 #include <math.h>
+#include "mpi.h"
 
 #define PI 3.1415926535
 
+double sin_integral(long long int start_count, long long int end_count, double rect_width){
+    double sum = 0;
+    long long int i;
+    for(i = start_count; i <= end_count; i++) {
+
+        /* find the middle of the interval on the X-axis. */
+
+        double x_middle = (i - 0.5) * rect_width;
+        double area = sin(x_middle) * rect_width;
+        sum = sum + area;
+    }
+    return sum;
+}
+
 int main(int argc, char **argv)
 {
-    long long i, num_intervals;
-    double rect_width, area, sum, x_middle;
+    MPI_Init(&argc, &argv);
+
+    int mpi_local_rank, mpi_local_size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &mpi_local_rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &mpi_local_size);
+
+    long long num_intervals;
+    double rect_width;
 
     sscanf(argv[1],"%llu",&num_intervals);
 
     rect_width = PI / num_intervals;
 
-    sum = 0;
-    for(i = 1; i < num_intervals + 1; i++) {
-
-        /* find the middle of the interval on the X-axis. */
-
-        x_middle = (i - 0.5) * rect_width;
-        area = sin(x_middle) * rect_width;
-        sum = sum + area;
+    long long int mpi_local_start_count = 1;
+    long long int mpi_local_end_count = num_intervals;
+    if( mpi_local_rank != 0 ){
+        mpi_local_start_count = (mpi_local_rank * num_intervals / mpi_local_size) + 1;
+    }
+    if( mpi_local_rank != mpi_local_size - 1 ){
+        mpi_local_end_count = (mpi_local_rank+1) * num_intervals / mpi_local_size;
     }
 
-    printf("The total area is: %f\n", (float)sum);
+    double mpi_local_sum = sin_integral(mpi_local_start_count, mpi_local_end_count, rect_width);
+    double sum = 0;
+
+    int err = MPI_Reduce(&mpi_local_sum, &sum, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+    if( err != MPI_SUCCESS ){
+        perror("MPI_Reduce Error!\n");
+    }
+
+    MPI_Barrier(MPI_COMM_WORLD);
+    if( mpi_local_rank == 0 ){
+        printf("The total area is: %.16f\n", sum);
+    }
 
     return 0;
 }
--- hw3/prime.c
+++ hw3/prime.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include "mpi.h"
 
 int isprime(int n) {
     int i,squareroot;
@@ -15,24 +16,81 @@ int isprime(int n) {
         return 0;
 }
 
-int main(int argc, char *argv[])
-{
-    int pc,       /* prime counter */
-        foundone; /* most recent prime found */
-    long long int n, limit;
+struct CountPrimeRet{
+    int prime_counter;
+    long long int max_prime;
+}; /* initial value {0, 0} */
 
-    sscanf(argv[1],"%llu",&limit);
-    printf("Starting. Numbers to be scanned= %lld\n",limit);
+struct CountPrimeRet count_prime(long long int start, long long int end){
+    struct CountPrimeRet ret = {0, 0};
+    if( start % 2 == 0 ){
+        start += 1;
+    }
 
-    pc=4;     /* Assume (2,3,5,7) are counted here */
-
-    for (n=11; n<=limit; n=n+2) {
+    long long int n;
+    for (n=start; n<=end; n=n+2) {
         if (isprime(n)) {
-            pc++;
-            foundone = n;
+            ret.prime_counter++;
+            ret.max_prime = n;
         }
     }
-    printf("Done. Largest prime is %d Total primes %d\n",foundone,pc);
+
+    return ret;
+}
+
+
+int main(int argc, char *argv[])
+{
+    int pc;       /* prime counter */
+    long long int limit,
+                  foundone; /* most recent prime found */
+
+    MPI_Init(&argc, &argv);
+
+    int mpi_local_rank, mpi_local_size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &mpi_local_rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &mpi_local_size);
+
+        sscanf(argv[1],"%llu",&limit);
+    if( mpi_local_rank == 0 ){
+        printf("Starting. Numbers to be scanned= %lld\n",limit);
+    }
+
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    long long int mpi_local_start = 11;
+    long long int mpi_local_end = limit;
+    if( mpi_local_rank != 0 ){
+        mpi_local_start = (mpi_local_rank * limit / mpi_local_size) + 1;
+    }
+    if( mpi_local_rank != mpi_local_size - 1 ){
+        mpi_local_end = (mpi_local_rank+1) * limit / mpi_local_size;
+    }
+
+#ifdef __DEBUG__
+    printf("start: %lld, end: %lld\n", mpi_local_start, mpi_local_end);
+#endif
+    struct CountPrimeRet prime_local_ans = count_prime(mpi_local_start, mpi_local_end);
+    struct CountPrimeRet prime_ans = {0, 0};
+
+    int err = MPI_Reduce(&prime_local_ans.prime_counter, &prime_ans.prime_counter, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+    if( err != MPI_SUCCESS ){
+        perror("MPI_Reduce Error!\n");
+    }
+
+    err = MPI_Reduce(&prime_local_ans.max_prime, &prime_ans.max_prime, 1, MPI_INT, MPI_MAX, 0, MPI_COMM_WORLD);
+    if( err != MPI_SUCCESS ){
+        perror("MPI_Reduce Error!\n");
+    }
+    
+    MPI_Barrier(MPI_COMM_WORLD);
+    if( mpi_local_rank == 0 ){
+        foundone = prime_ans.max_prime;
+        pc = prime_ans.prime_counter + 4;     /* Assume (2,3,5,7) are counted here */
+        printf("Done. Largest prime is %lld Total primes %d\n", foundone, pc);
+    }
+
+    MPI_Finalize();
 
     return 0;
 }
